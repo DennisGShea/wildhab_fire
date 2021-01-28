4eb4754000eb2b4399cdb27fd2220e30a4a1e490
Dennis G Shea <dgs@Denniss-MacBook-Pro.local>
2021-01-28 17:25:42 -0500

    changed port

---
+++ code/ToddWH/wildhab_fire-main/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const { getEvents, postEvent } = require('./src/events')
+const { getPeople, postPerson } = require('./src/users')
+const { getPerson, updatePerson } = require('./src/users/users.js')
+const { getSingleEvent, deleteEvent, updateEvent } = require('./src/events/eventId')
+
+const app = express()
+app.use(bodyParser.json())
+const port = 5001
+
+app.get('/events', getEvents)
+app.get('/events/:eventId', getSingleEvent)
+app.post('/events', postEvent)
+app.delete('/events/:eventId', deleteEvent)
+app.patch('/events/:eventId', updateEvent)
+
+app.get('/people', getPeople)
+app.post('/people', postPerson)
+app.get('/person/:personId', getPerson)
+app.patch('/person/:personId', updatePerson)
+
+app.get('/', (req, res) => {
+	res.status(200).send('Hello World!')
+})
+
+app.listen(port, () => {
+	console.log('listening on http://localhost:' + port)
+})
